f1e68445a027ece2d4936ecf6af5c1e8909a0d1e
isaacwkz <54544858+isaacwkz@users.noreply.github.com>
2020-09-16 23:57:04 +0800

    Added camera functions
    TODO:
    1. Save photos to userspace
    2. Debug photo taking function in emulator
    3. Add photo gallery into app

--- lib/Screen/camera.dart
+++ lib/Screen/camera.dart
@@ -4,14 +4,17 @@ import 'package:intl/intl.dart';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:swipedetector/swipedetector.dart';
-import 'package:flutter_app_badger/flutter_app_badger.dart';
+import 'package:camera/camera.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+import 'dart:typed_data';
+import 'package:esys_flutter_share/esys_flutter_share.dart';
 
 //close Camscanner and go back to calendar
 class openCalendar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    /*return Scaffold(
       appBar: AppBar(
         title: Text("Camera Scanner"),
       ),
@@ -24,6 +27,288 @@ class openCalendar extends StatelessWidget {
           child: Text('Go back!'),
         ),
       ),
+    );*/
+
+    return MaterialApp(
+      title: 'Uree',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Rubik',
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: CameraScreen(),
     );
+
+  }
+}
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  CameraController cameraController;
+  List cameras;
+  int selectedCameraIndex;
+  String imgPath;
+
+
+
+  Future initCamera(CameraDescription cameraDescription) async {
+    if (cameraController != null) {
+      await cameraController.dispose();
+    }
+
+    cameraController =
+        CameraController(cameraDescription, ResolutionPreset.high);
+
+    cameraController.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+    });
+
+    if (cameraController.value.hasError) {
+      print('Camera Error ${cameraController.value.errorDescription}');
+    }
+
+    try {
+      await cameraController.initialize();
+    } catch (e) {
+      showCameraException(e);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  /// Display camera preview
+  Widget cameraPreview() {
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return Text(
+        'Loading',
+        style: TextStyle(
+            color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
+      );
+    }
+
+    return AspectRatio(
+      aspectRatio: cameraController.value.aspectRatio,
+      child: CameraPreview(cameraController),
+    );
+  }
+
+  Widget cameraControl(context) {
+    return Expanded(
+      child: Align(
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            FloatingActionButton(
+              child: Icon(
+                Icons.camera,
+                color: Colors.black,
+              ),
+              backgroundColor: Colors.white,
+              onPressed: () {
+                onCapture(context);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget cameraToggle() {
+    if (cameras == null || cameras.isEmpty) {
+      return Spacer();
+    }
+
+    CameraDescription selectedCamera = cameras[selectedCameraIndex];
+    CameraLensDirection lensDirection = selectedCamera.lensDirection;
+
+    return Expanded(
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: FlatButton.icon(
+            onPressed: () {
+              onSwitchCamera();
+            },
+            icon: Icon(
+              getCameraLensIcons(lensDirection),
+              color: Colors.white,
+              size: 24,
+            ),
+            label: Text(
+              '${lensDirection.toString().substring(lensDirection.toString().indexOf('.') + 1).toUpperCase()}',
+              style:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+            )),
+      ),
+    );
+  }
+
+  onCapture(context) async {
+    try {
+      final p = await getTemporaryDirectory();
+      final name = DateTime.now();
+      final path = "${p.path}/$name.png";
+
+      await cameraController.takePicture(path).then((value) {
+        print('here');
+        print(path);
+        Navigator.push(context, MaterialPageRoute(builder: (context) =>PreviewScreen(imgPath: path,fileName: "$name.png",)));
+      });
+
+    } catch (e) {
+      showCameraException(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    availableCameras().then((value) {
+      cameras = value;
+      if(cameras.length > 0){
+        setState(() {
+          selectedCameraIndex = 0;
+        });
+        initCamera(cameras[selectedCameraIndex]).then((value) {
+
+        });
+      } else {
+        print('No camera available');
+      }
+    }).catchError((e){
+      print('Error : ${e.code}');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        child: Stack(
+          children: <Widget>[
+//            Expanded(
+//              flex: 1,
+//              child: _cameraPreviewWidget(),
+//            ),
+            Align(
+              alignment: Alignment.center,
+              child: cameraPreview(),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 120,
+                width: double.infinity,
+                padding: EdgeInsets.all(15),
+                color: Colors.transparent,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    cameraToggle(),
+                    cameraControl(context),
+                    Spacer(),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  getCameraLensIcons(lensDirection) {
+    switch (lensDirection) {
+      case CameraLensDirection.back:
+        return CupertinoIcons.switch_camera;
+      case CameraLensDirection.front:
+        return CupertinoIcons.switch_camera_solid;
+      case CameraLensDirection.external:
+        return CupertinoIcons.photo_camera;
+      default:
+        return Icons.device_unknown;
+    }
+  }
+
+  onSwitchCamera() {
+    selectedCameraIndex =
+    selectedCameraIndex < cameras.length - 1 ? selectedCameraIndex + 1 : 0;
+    CameraDescription selectedCamera = cameras[selectedCameraIndex];
+    initCamera(selectedCamera);
+  }
+
+  showCameraException(e) {
+    String errorText = 'Error ${e.code} \nError message: ${e.description}';
+  }
+}
+
+class PreviewScreen extends StatefulWidget {
+  final String imgPath;
+  final String fileName;
+  PreviewScreen({this.imgPath, this.fileName});
+
+  @override
+  _PreviewScreenState createState() => _PreviewScreenState();
+}
+
+class _PreviewScreenState extends State<PreviewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: true,
+        ),
+        body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Image.file(File(widget.imgPath),fit: BoxFit.cover,),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: double.infinity,
+                  height: 60,
+                  color: Colors.black,
+                  child: Center(
+                    child: IconButton(
+                      icon: Icon(Icons.share,color: Colors.white,),
+                      onPressed: (){
+                        getBytes().then((bytes) {
+                          print('here now');
+                          print(widget.imgPath);
+                          print(bytes.buffer.asUint8List());
+                          Share.file('Share via', widget.fileName, bytes.buffer.asUint8List(), 'image/path');
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+    );
+  }
+
+  Future getBytes () async {
+    Uint8List bytes = File(widget.imgPath).readAsBytesSync() as Uint8List;
+//    print(ByteData.view(buffer))
+    return ByteData.view(bytes.buffer);
   }
 }
