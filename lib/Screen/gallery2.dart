525d8fc4243da610397bac5f8d68d08d6d7b65dd
isaacwkz <54544858+isaacwkz@users.noreply.github.com>
2020-10-21 14:46:13 +0800

    Added new branch

---
+++ lib/Screen/gallery2.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:typed_data';
+import 'package:image_picker/image_picker.dart';
+import 'package:dip_taskplanner/Screen/camera.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+import 'package:flutter_svg/svg.dart';
+
+// Entry point into the gallery
+class GalleryPageEntry extends StatefulWidget {
+  @override
+  _GalleryExampleState createState() => _GalleryExampleState();
+}
+
+class GalleryExample extends StatefulWidget {
+  @override
+  _GalleryExampleState createState() => _GalleryExampleState();
+}
+
+class _GalleryExampleState extends State<GalleryExample> {
+
+}
